bb3368d2262a35ea5aebc61affd5537c594f8e4b
BayezidMostafa <hiperx1@gmail.com>
2022-10-12 08:07:55 +0600

    Quiz Counter Added

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaceFrownIcon } from '@heroicons/react/24/solid'
 import { Nav } from '../Nav/Nav';
 
 
--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -3,6 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 import Question from '../Question/Question';
 
 const QuizDetails = () => {
+
     const quizDetails = useLoaderData()
     const {name, questions} = quizDetails.data;
 
@@ -11,7 +12,7 @@ const QuizDetails = () => {
             <p style={{textShadow: '1px 1px 3px black'}} className='bg-violet-900 text-xl md:text-2xl xl:text-3xl lg:text-4xl font-bold w-4/5 md:w-3/5 mx-auto py-5 mb-20 rounded-xl shadow shadow-black text-white'>You Are In {name} Quiz Section! You Can Do It!</p>
             <div>
                 {
-                    questions.map(singleQuestion => <Question key={singleQuestion.id}  singleQuestion={singleQuestion}/>)
+                    questions.map((singleQuestion, index) => <Question key={singleQuestion.id} index={index}  singleQuestion={singleQuestion}/>)
                 }
             </div>
         </div>
