0f049c3afe3a4e392805cbff6ecf2da828cdf01f
BayezidMostafa <hiperx1@gmail.com>
2022-10-12 20:48:01 +0600

    Finalized The Whole Code And Increased Result Timer

--- src/components/QuizTopic/QuizTopic.js
+++ src/components/QuizTopic/QuizTopic.js
@@ -7,7 +7,7 @@ const QuizTopic = () => {
     return (
         <div className='mt-10'>
             <p className='text-3xl md:text-4xl lg:text-5xl font-bold' style={{textShadow: '1px 1px 3px black'}}>Here I Got Some Quiz Topics For You!</p>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 container mx-auto  my-24 w-3/4 md:w-full'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 container mx-auto  my-24 w-5/6 md:w-full'>
                 {
                     quizTopic.data.map(quiz => <Quiz key={quiz.id} quiz={quiz}/>)
                 }
